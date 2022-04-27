162102b83037ed2a222ba8869a350b4910695758
Vo Dinh Hoang Long <cyberpunk2076@gmail.com>
2022-04-27 16:57:50 +0700

    add reposity

---
+++ src/app/repositories/user.repository.js
@@ -0,0 +1,16 @@
+const db = require("../models/configDb");
+const Tutorial = db.tutorials;
+
+module.exports = Object.freeze({
+  create: async (data) => Tutorial.create(data),
+  findById: async (id) => Tutorial.findByPk(id),
+  findAll: async (condition) => Tutorial.findAll({ where: condition }),
+  removeById: async (id) =>
+    Tutorial.destroy({
+      where: { id: id },
+    }),
+  update: async (data, id) =>
+    Tutorial.update(data, {
+      where: { id: id },
+    }),
+});
